7703377686ed9213aa0d4eb0618b0ef3a9518091
caduxl007 <eduardo2020dev@gmail.com>
2023-07-20 00:17:55 -0300

    feat: add component Sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { UserButton } from '@clerk/nextjs'
 export default function Dashboard() {
   return (
     <div>
-      <h1 className="text-6xl">Hello Word</h1>
+      <h1 className="text-6xl text-green-500">Hello Word</h1>
 
       <UserButton afterSignOutUrl="/" />
     </div>
